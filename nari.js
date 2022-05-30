116723b2fd5afdf476a4fdecffdb9c9f6301fd15
mahinari965 <mahendranaik803@gmail.com>
2022-05-30 07:11:01 +0530

    on day work

---
+++ nari.js
@@ -0,0 +1,26 @@
+ var a=12;
+
+ var b=23;
+
+ function add(a,b) {
+     total=a+b;
+
+     console.log(total);
+     
+ }
+ add();
+ console.log(a,"",b);
+
+
+ var a="mahendra";
+ var b="naik";
+ 
+ function add() {
+     sum=a+b;
+     console.log(sum);
+     
+ };
+
+ add();
+
+ console.log(a,"",b);
